7b8d0c64d62765a906c1a0b6f3b6952e6a4ba864
pavel <pavel.vizir@gmail.com>
2018-03-02 11:54:48 +0300

    small fixes

--- datetime_practice.py
+++ datetime_practice.py
@@ -12,25 +12,25 @@ from datetime import date, datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
 
-def print_dates():
+def get_dates():
     '''1. Напечатайте в консоль даты: вчера, сегодня, месяц назад.'''
-    date_today = date.today()
-    date_yesterday = date_today + timedelta(days=-1)
-    date_month_ago = date_today + relativedelta(months=-1)
-    print()
-    print("{:20s}{:>20}".format("Сегодня: ", str(date_today)))
-    print("{:20s}{:>20}".format("Вчера: ", str(date_yesterday)))
-    print("{:20s}{:>20}".format("Месяц назад: ", str(date_month_ago)))
+    dates =    {}
+    dates["Сегодня"]    =    date.today()
+    dates["Вчера"] =    dates["Сегодня"] + timedelta(days=-1)
+    dates["Месяц назад"]    =    dates["Сегодня"]    + relativedelta(months=-1)
+    return '\n'.join(["{:20}{:>20}".format(k+":",str(v)) for k,v in dates.items()]), dates
 
 
-def convert_string_to_date(string):
+def convert_string_to_date(string=None):
     '''2. Превратите строку "01/01/17 12:10:03.234567" в объект datetime.'''
-
+    if string is None:
+        string =    "01/01/17 12:10:03.234567"
     return datetime.strptime(string, '%m/%d/%y %H:%M:%S.%f')
 
 
-print_dates()
+print(get_dates()[0])
+print("\n",get_dates()[1]["Вчера"],"\n")
 
-datetime_string = "01/01/17 12:10:03.234567"
-datetime_from_string = convert_string_to_date(datetime_string)
-print("\n\t{}\n\t{}".format(type(datetime_from_string), str(datetime_string)))
+for arg in [None, "02/03/19 19:30:03.234567"]:
+    i = convert_string_to_date(arg)
+    print("\t{}\n\t{}".format(i, type(i)))
