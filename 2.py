a9efebd37da4b658a7e6bdd528d03a6fa7c33e7f
pavel <pavel.vizir@gmail.com>
2018-03-07 07:54:52 +0300

    small fixes android

--- 2.py
+++ 2.py
@@ -9,32 +9,28 @@
 from csv import reader
 from json import loads
 
-from geopy.distance import distance
-# from geopy.distance import VincentyDistance
-from math import hypot
-from itertools import permutations
-
+from geopy.distance import VincentyDistance
+#from math import abs
+from datetime import datetime
 
 file_name_bus = 'data-398-2018-02-13.csv'
 file_name_metro = 'data-397-2018-02-27.json'
 delim = ';'
 enc = 'windows-1251'
-radius = 500
 
 
 def get_bus_stops(file_name, encoding, delimiter):
-    ''' Получаем набор (широта, долгота) остановок (3 и 2 столбцы). '''
+    ''' Получаем набор координат остановок (2 и 3 столбцы). '''
     with open(file_name, 'r', encoding=encoding) as file:
         content = reader(file, delimiter=delimiter)
         next(content)  # Пропускаем заголовки
-        result = sorted(
-            set((float(line[3]), float(line[2])) for line in content))
+        result = set((float(line[2]), float(line[3])) for line in content)
 
         return result
 
 
 def get_metro_exits(file_name, encoding):
-    ''' Получаем словарь из станций и списка (широта, долгота) выходов. '''
+    ''' Получаем словарь из станций и списка координат выходов. '''
     with open(file_name, 'r', encoding=encoding) as file:
         content = loads(file.read())
         result = {}
@@ -43,42 +39,35 @@ def get_metro_exits(file_name, encoding):
             lon, lat = line['geoData']['coordinates']
             station = line['NameOfStation']
             try:
-                result[station].append((lat, lon))
+                result[station].append((lon, lat))
             except KeyError:
-                result[station] = [(lat, lon), ]
+                result[station] = [(lon, lat), ]
 
         return result
 
 
-def get_max_bus_stops(metro, bus):
+def get_max_bus_stops(metro_coord, bus_coord):
     ''' Возвращает максимальное количество остановок
         и список станций в отформатированном виде. '''
-    max_lat = max([lat for lat, lon in bus])
-    # Длина градуса долготы на верхней границе широт в словаре автобусов.
-    lon_m = radius / distance((max_lat, 0), (max_lat, 1)).m
-    lat_m = radius / 110500  # Нижняя граница длины градуса широты 110.574 км.
-    result = [0, []]
+    lat_m = 500/111000
+    lon_m = 500/63000
+    result = list([0, ])
     counted_for_station = set()
 
-    for station, coord_list in metro.items():
+    for station, coord_list in metro_coord.items():
         counter = 0
-#        print(len(list(permutations(coord_list))))
-        for station_exit in coord_list:
-            slat = station_exit[0]
-            slon = station_exit[1]
+        #print(station)
 
-            for bus_stop_coord in bus:
-                lat_diff = bus_stop_coord[0] - slat
-                if lat_diff > lat_m:
-                    break
-                elif (
-                        abs(lat_diff) < lat_m and
-                        abs(bus_stop_coord[1] - slon) < lon_m and
-                        bus_stop_coord not in counted_for_station and
-                        distance(bus_stop_coord, station_exit).m <= radius
-                ):
-                    counted_for_station.add(bus_stop_coord)
-                    counter += 1
+        for station_exit in coord_list:
+            slat =    station_exit[1]
+            slon =    station_exit[0]
+            for bus_stop_coord in bus_coord:
+                if abs(bus_stop_coord[0] - slon) < lon_m:
+                    #if  abs(bus_stop_coord[1] - slat) < lat_m:
+                        if bus_stop_coord not in counted_for_station:
+                            if VincentyDistance(bus_stop_coord, station_exit).m <= 500:
+                                counted_for_station.add(bus_stop_coord)
+                                counter += 1
         counted_for_station.clear()
 
         if counter > result[0]:
@@ -91,9 +80,8 @@ def get_max_bus_stops(metro, bus):
 
     return result
 
-
 bus_stops = get_bus_stops(file_name_bus, enc, delim)
 metro_exits = get_metro_exits(file_name_metro, enc)
+w =    datetime.now()
 max_bus_stop = get_max_bus_stops(metro_exits, bus_stops)
-print(max_bus_stop)
-#print(bus_stops)
+print(max_bus_stop, datetime.now() - w)
